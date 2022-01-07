8ede515e2873d8e6931305785a927521dcae8ec7
Ckeesee <codman1821@gmail.com>
2022-01-07 11:42:52 -0600

    BooleanLogicAssignment

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Car Insurance Approval:");
+            Console.WriteLine("What is your age?");
+            string ageinput = Console.ReadLine();
+            int age = Convert.ToInt32(ageinput);
+
+            Console.WriteLine("Have you ever had a DUI? (Yes/No)");
+            string DUI = Console.ReadLine();
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string ticketInput = Console.ReadLine();
+            int tickets = Convert.ToInt32(ticketInput);
+
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(age > 15 && DUI == "No" && tickets <= 3);
+
+            Console.ReadLine();
+        }
+    }
+}
