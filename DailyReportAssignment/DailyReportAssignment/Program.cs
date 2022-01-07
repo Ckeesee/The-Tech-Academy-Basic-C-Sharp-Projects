6e62c580521966f47dae7bc53a5d6232a4d14933
Ckeesee <codman1821@gmail.com>
2022-01-07 11:56:50 -0600

    dailyreportrevision

--- DailyReportAssignment/DailyReportAssignment/Program.cs
+++ DailyReportAssignment/DailyReportAssignment/Program.cs
@@ -15,15 +15,15 @@ namespace DailyReportAssignment
             Console.WriteLine("What course are you on?");
             string yourCourse = Console.ReadLine();
             Console.WriteLine("What page number?");
-            string  yourPage = Console.ReadLine();
+            int  yourPage = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
-            string helpRequest = Console.ReadLine();
+            bool helpRequest = Convert.ToBoolean(Console.ReadLine());
             Console.WriteLine("Were there any positive experiences you'd like to share? \n Please give specifics.");
             string positiveExp = Console.ReadLine();
             Console.WriteLine("Is there any other feedback you'd like to provide? \n Please be specific.");
             string feedback = Console.ReadLine();
             Console.WriteLine("How many hours did you study today?");
-            string hoursStudied = Console.ReadLine();
+            int hoursStudied = Convert.ToInt32(Console.ReadLine());
 
             //closing statement
             Console.WriteLine("Thank you for your answers! \nAn Instructor will respond to this shortly. \nHave a great day!");
