b769c60e00e50608286cc0aee4f528a9a33fd0bb
Ckeesee <codman1821@gmail.com>
2022-01-17 12:54:07 -0600

    overload method assignment

---
+++ MainMethodAssignment/MainMethodAssignment/Operations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainMethodAssignment
+{
+    class Operations
+    {
+        public static int math(decimal num)
+        {
+            int total = Convert.ToInt32(num) / 10;
+            return total;
+        }
+
+        public static int math(string num)
+        {
+            int total = Convert.ToInt32(num) * 7;
+            return total;
+        }
+
+        public static int math(int num)
+        {
+            int total = num + 32;
+            return total;
+        }
+
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What integer would you like to do an addition operation on?");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What decimal would you like to do a division operation on?");
+            decimal num2 = Decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("What number would you like to do and multiplication operation on?");
+            string num3 = Console.ReadLine();
+            //adding the int by using the sum method
+            Console.WriteLine(Operations.math(num) + " this is your number added by 32");
+            //dividing the int number using the Divide method
+            Console.WriteLine(Operations.math(num2) + " this is your number divided by 10");
+            //multiplying the int using the mulitply method
+            Console.WriteLine(Operations.math(num3) + " this is your number multiplied by 7");
+            
+
+            Console.ReadLine();
+        }
+    }
+}
