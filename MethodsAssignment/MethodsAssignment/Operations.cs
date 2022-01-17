bbfe1df0c3db95812a034a88d015f10db874cb35
Ckeesee <codman1821@gmail.com>
2022-01-17 12:07:32 -0600

    Methods assignment

---
+++ MethodsAssignment/MethodsAssignment/Operations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodsAssignment
+{
+    class Operations
+    {
+        public static int Divide(int num)
+        {
+            int total = num / 10;
+            return total;
+        }
+
+        public static int Multiply(int num)
+        {
+            int total = num * 7;
+            return total;
+        }
+
+        public static int Sum(int num)
+        {
+            int total = num + 32;
+            return total;
+        }
+
+    }
+}
---
+++ MethodsAssignment/MethodsAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number would you like to do math operations on?");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            //dividing the int number using the Divide method
+            Console.WriteLine(Operations.Divide(num) + " this is your number divided by 10");
+            //multiplying the int using the mulitply method
+            Console.WriteLine(Operations.Multiply(num) + " this is your number multiplied by 7");
+            //adding the int by using the sum method
+            Console.WriteLine(Operations.Sum(num) + " this is your number added by 32");
+
+            Console.ReadLine();
+        }
+
+    }
+}
