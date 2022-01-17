966132ac8eca02a376a1a63a9e60caf31f506d9c
Ckeesee <codman1821@gmail.com>
2022-01-17 13:19:16 -0600

    optional paramter assignment

---
+++ optionalParamatersAssignment/optionalParamatersAssignment/Operations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace optionalParamatersAssignment
+{
+    class Operations
+    {
+        public static int math(int num, int num2 = 1)
+        {
+            int total = num * num2;
+            return total;
+        }
+
+ 
+    }
+}
---
+++ optionalParamatersAssignment/optionalParamatersAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace optionalParamatersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num2;
+            int result;
+            Console.WriteLine("What integer would you like to do a math operation on?");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What second number would you like to do a math operation on? (This is optional):");
+            if (int.TryParse(Console.ReadLine(), out num2))
+                result = Operations.math(num, num2);
+            else
+                result = Operations.math(num);
+
+            //adding the int by using the sum method
+            Console.WriteLine( "Your result equals: " + result);
+          
+            
+            Console.ReadLine();
+        }
+    
+    }
+}
