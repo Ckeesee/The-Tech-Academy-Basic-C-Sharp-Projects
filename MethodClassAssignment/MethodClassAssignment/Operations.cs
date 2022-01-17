7b0f4e7fb3d091e1dbeb39982af3a6f2bbe37b8f
Ckeesee <codman1821@gmail.com>
2022-01-17 14:19:39 -0600

    naming doubel paramaters

---
+++ MethodClassAssignment/MethodClassAssignment/Operations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodClassAssignment
+{
+    public class Operations
+    {
+        public  void Mathmatics(int multiply, int show)
+        {
+            int result = multiply * 100;
+            Console.WriteLine("Here is the first number times 100: " + result);
+
+            Console.WriteLine("Here is the second number: " + show);
+
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations math = new Operations();
+            math.Mathmatics(8, 5);
+
+            math.Mathmatics(multiply: 10, show: 8);
+
+            Console.ReadLine();
+        }
+    }
+}
