23f9ffce048fc58071172c1de82f4fcda9bf3c49
Ckeesee <codman1821@gmail.com>
2022-01-20 13:24:15 -0600

    methodsassignemnt

---
+++ ClassSubmissionAssignment/ClassSubmissionAssignment/Ops.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassSubmissionAssignment
+{
+    public class Ops
+    {
+        //void method
+        public void Divide(int num1)
+        {
+            int total = num1 / 2;
+            Console.WriteLine("Here is your number divided by 2: " + total);
+        }
+
+        //static class
+        public static int Multiply(int num1, int num2)
+        {
+            int product = num1 * num2;
+            return product;
+        }
+
+        public static int Fave(out int favNum)
+        {
+            favNum = 21;
+            favNum += 100;
+            return favNum;
+
+        }
+    
+
+    //overloaded methods
+        public static int Operation(decimal num)
+            {
+                int total = Convert.ToInt32(num) / 10;
+                return total;
+            }
+
+            public static int Operation(string num)
+            {
+                int total = Convert.ToInt32(num) * 7;
+                return total;
+            }
+
+            public static int Operation(int num)
+            {
+                int total = num + 32;
+                return total;
+            }
+
+
+    }
+}
---
+++ ClassSubmissionAssignment/ClassSubmissionAssignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassSubmissionAssignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int favNum = 0;
+            Ops math = new Ops();
+            Console.WriteLine("What integer would you like to do a math operation on?");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            math.Divide(num);
+
+            //overloaded call paramaters
+            Console.WriteLine("What integer would you like to do an addition operation on?");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What decimal would you like to do a division operation on?");
+            decimal num2 = Decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("What number would you like to do and multiplication operation on?");
+            string num3 = Console.ReadLine();
+            //adding the int by using the sum method
+            Console.WriteLine(Ops.Operation(num1) + " this is your number added by 32");
+            //dividing the int number using the Divide method
+            Console.WriteLine(Ops.Operation(num2) + " this is your number divided by 10");
+            //multiplying the int using the mulitply method
+            Console.WriteLine(Ops.Operation(num3) + " this is your number multiplied by 7");
+
+            //static method calling
+            Console.WriteLine("Here is 10 times 43: " + Ops.Multiply(10, 43));
+
+            Console.WriteLine("Here is my favorite number! " + Ops.Fave(out favNum));
+
+            Console.ReadLine();
+        }
+    }
+            
+}
