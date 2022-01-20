0d8a10467132a52fc06b7fc9158c402d07ae6e9c
Ckeesee <codman1821@gmail.com>
2022-01-20 14:13:10 -0600

    inheritenceassign

---
+++ inheritence/inheritence/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace inheritence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee temp = new Employee() { FirstName = "Sample", LastName = "Student" };
+
+            temp.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}
