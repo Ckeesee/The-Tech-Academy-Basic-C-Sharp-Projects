4dc62d6191a03f8600a2f0630a7550927b3ad0cd
Ckeesee <codman1821@gmail.com>
2022-01-20 16:13:50 -0600

    overload ops

--- AbstractClassAssign/AbstractClassAssign/Employee.cs
+++ AbstractClassAssign/AbstractClassAssign/Employee.cs
@@ -9,6 +9,7 @@ namespace AbstractClassAssign
 {
     public class Employee : Person, IQuittable
     {
+        public int Id { get; set; }
         public override void SayName()
         {
             base.SayName();
@@ -17,6 +18,20 @@ namespace AbstractClassAssign
         public void Quit()
         {
             Console.WriteLine("This job isn't working for me!");
+        }
+        public static bool operator==(Employee employee,Employee employee2)
+        {
+            if (employee.Id == employee2.Id)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator!=(Employee employee, Employee employee2)
+        {
+            if (employee.Id != employee2.Id)
+                return false;
+            else
+                return true;
         }
     }
 }
