143a473326dcef96ed963ec1ffc3286f44803181
Ckeesee <codman1821@gmail.com>
2022-01-20 15:18:23 -0600

    abstractassign

---
+++ AbstractClassAssign/AbstractClassAssign/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AbstractClassAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee temp = new Employee() { firstName = "Sample", lastName = "Student" };
+            temp.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}
