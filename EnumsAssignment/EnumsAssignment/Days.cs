f2f57a7d73043f38fc10b764e9c758e774b09fa9
Ckeesee <codman1821@gmail.com>
2022-01-24 13:54:52 -0600

    enums assignment

---
+++ EnumsAssignment/EnumsAssignment/Days.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnumsAssignment
+{
+    class Days
+    {
+        public enum Week
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturaday,
+            Sunday
+        }
+
+    }
+}
---
+++ EnumsAssignment/EnumsAssignment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace EnumsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            try
+            {
+                Console.WriteLine("What is the current day of the week?!");
+                Week pick = (Week)Enum.Parse(typeof(Week), Console.ReadLine());
+
+                Console.WriteLine(pick + " is your favorite day of the week!");
+
+   
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter a valid day of the week.");
+                Week pick = (Week)Enum.Parse(typeof(Week), Console.ReadLine());
+                
+            }
+            
+        }
+        public enum Week
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturaday,
+            Sunday
+        }
+    }
+}
