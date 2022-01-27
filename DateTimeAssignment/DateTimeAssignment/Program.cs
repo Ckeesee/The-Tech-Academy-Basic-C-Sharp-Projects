38b5a6d720a84b60df79fc3c91d89189cc1702df
Ckeesee <codman1821@gmail.com>
2022-01-27 12:56:20 -0600

    date time assignement

---
+++ DateTimeAssignment/DateTimeAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.IO;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = new DateTime();
+            Console.WriteLine("The current date and time is: {0}", DateTime.Now);
+
+            Console.WriteLine("Please pick a number!");
+            double userPick = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Here is the time with {0} hours addded to the current time: {1}", userPick, DateTime.Now.AddHours(userPick));
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
