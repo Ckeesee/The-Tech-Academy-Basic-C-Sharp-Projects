3031e62d6b75fe3283b4ad27f36e34b7aab8b537
Ckeesee <codman1821@gmail.com>
2022-02-03 13:30:42 -0600

    asignment

---
+++ CarInsurance/CarInsurance/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using CarInsurance.Models;
+using CarInsurance.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsurance.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Admin()
+        {
+            using (InsuranceEntities db = new InsuranceEntities())
+            {
+                //var signups = db.SignUps.Where(x => x.Removed == null).ToList();
+                var insuree = db.Insurees;
+                var insureeVms = new List<InsureeVm>();
+                foreach (var client in insuree)
+                {
+                    var insureeVm = new InsureeVm();
+                    insureeVm.Id = client.Id;
+                    insureeVm.FirstName = client.FirstName;
+                    insureeVm.LastName = client.LastName;
+                    insureeVm.EmailAddress = client.EmailAddress;
+                    insureeVm.Quote = client.Quote;
+                    insureeVms.Add(insureeVm);
+                }
+                return View(insureeVms);
+            }
+           
+        }
+    }
+}
