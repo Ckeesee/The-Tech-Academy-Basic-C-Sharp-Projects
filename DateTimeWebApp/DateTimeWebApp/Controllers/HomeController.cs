25a24784db80d47afbf855f471a2f37b20ef16ec
Ckeesee <codman1821@gmail.com>
2022-02-12 02:54:38 -0600

    DATETIMEAPP

---
+++ DateTimeWebApp/DateTimeWebApp/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using DateTimeWebApp.Models;
+using DateTimeWebApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DateTimeWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var date = new DateTimeVm();
+            date.CurrentDate();
+            return View(date);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DateTimeWebApp/DateTimeWebApp/ViewModels/DateTimeVm.cs
@@ -0,0 +1,13 @@
+﻿namespace DateTimeWebApp.ViewModels
+{
+    public class DateTimeVm
+    {
+        public DateTime Date { get; set; }
+        public DateTime Time { get; set; }
+
+        public void CurrentDate()
+        {
+            Date = DateTime.Now;
+        }
+    }
+}
