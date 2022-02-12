b6566a77c0f0af39ce7da026026ecb4a97a7fe80
Ckeesee <codman1821@gmail.com>
2022-02-12 02:40:29 -0600

    codefirst assignment

---
+++ CodeFirstApp/CodeFirstApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace CodeFirstApp
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Mark" };
+
+                
+                db.Students.Add(stud);
+                db.SaveChanges();
+            }
+        }
+    }
+
+
+}
